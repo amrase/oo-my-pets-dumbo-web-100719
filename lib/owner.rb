35add9764d7456e70eb8306747d9c4daefae2415
Amra Sezairi <amra.sezairi@hotmail.com>
2019-10-15 04:53:44 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,13 +1,19 @@
 class Owner
   # attr_accessor :name
   attr_reader :species, :name
+  @@all =[]
   
   def initialize(species)
     @name=name
     @species="human"
+    @@all << self
   end
   
   def say_species
     "I am a #{@species}."
+  end  
+  
+  def self.all
+    @@all
   end  
 end
