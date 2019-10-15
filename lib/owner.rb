6d024b6d13a885de9913dcd76ecb6667473e2ce0
Amra Sezairi <amra.sezairi@hotmail.com>
2019-10-15 04:54:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,11 +2,13 @@ class Owner
   # attr_accessor :name
   attr_reader :species, :name
   @@all =[]
+  @@count=0 
   
   def initialize(species)
     @name=name
     @species="human"
     @@all << self
+    @@count+=1
   end
   
   def say_species
